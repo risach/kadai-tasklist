1d06d2ee5ffb3e64272bffff5169c6afab4c13f3
risa <risa_ssg63@yahoo.co.jp>
2020-10-31 12:33:10 +0000

    show fix

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,9 +16,10 @@
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    <td>{{ $task->id }}</td>
+                    
+                 {{-- メッセージ詳細ページへのリンク --}}
+                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
-           
                 </tr>
                 @endforeach
             </tbody>
