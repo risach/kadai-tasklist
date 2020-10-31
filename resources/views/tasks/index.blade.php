54994d5f62f54e3ea8159f779ac5f0fddfd280ea
risa <risa_ssg63@yahoo.co.jp>
2020-10-31 09:41:38 +0000

    delete

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -18,9 +18,7 @@
                 <tr>
                     <td>{{ $task->id }}</td>
                     <td>{{ $task->content }}</td>
-                     {{-- メッセージ詳細ページへのリンク --}}
-                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
-                    <td>{{ $task->content }}</td>
+           
                 </tr>
                 @endforeach
             </tbody>
